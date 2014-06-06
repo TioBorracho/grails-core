c4e2f89366862a978ee950886a6acf7fb350c79d
Darío Alejandro Guzik <dario@guzik.com.ar>
2014-06-06 17:26:11 -0300

    Option to disable hidden method parameter
    Due to security reasons one might want to disable this

--- grails-plugin-controllers/src/main/groovy/org/codehaus/groovy/grails/plugins/web/ControllersGrailsPlugin.groovy
+++ grails-plugin-controllers/src/main/groovy/org/codehaus/groovy/grails/plugins/web/ControllersGrailsPlugin.groovy
@@ -1,32 +1,4 @@
-/*
- * Copyright 2004-2005 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.codehaus.groovy.grails.plugins.web
-
-import grails.artefact.Enhanced
-import grails.util.Environment
-import grails.util.GrailsUtil
-import grails.util.GrailsWebUtil
-import groovy.transform.CompileStatic
-import org.codehaus.groovy.grails.commons.ControllerArtefactHandler
-import org.codehaus.groovy.grails.commons.DomainClassArtefactHandler
-import org.codehaus.groovy.grails.commons.GrailsDomainClass
-import org.codehaus.groovy.grails.commons.metaclass.MetaClassEnhancer
-import org.codehaus.groovy.grails.plugins.web.api.ControllersApi
-import org.codehaus.groovy.grails.plugins.web.api.ControllersDomainBindingApi
-import org.codehaus.groovy.grails.web.errors.GrailsExceptionResolver
+hceptionResolver
 import org.codehaus.groovy.grails.web.filters.HiddenHttpMethodFilter
 import org.codehaus.groovy.grails.web.mapping.UrlMappings
 import org.codehaus.groovy.grails.web.metaclass.RedirectDynamicMethod
@@ -213,9 +185,11 @@ class ControllersGrailsPlugin implements ServletContextInitializer{
         charEncoder.urlPatterns = catchAllMapping
         charEncoder.onStartup(servletContext)
 
-        def hiddenHttpFilter = new FilterRegistrationBean(new HiddenHttpMethodFilter())
-        hiddenHttpFilter.urlPatterns = catchAllMapping
-        hiddenHttpFilter.onStartup(servletContext)
+        if (!application.config.grails.disableHiddenHttpMethodFilter) {
+            def hiddenHttpFilter = new FilterRegistrationBean(new HiddenHttpMethodFilter())
+            hiddenHttpFilter.urlPatterns = catchAllMapping
+            hiddenHttpFilter.onStartup(servletContext)
+        }
 
         def webRequestFilter = new FilterRegistrationBean(new GrailsWebRequestFilter())
 
